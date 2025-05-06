f16933110a65adec47e05457dad80c48d729735a
Viktor Shvidkiy <vikt.sh@gmail.com>
2025-05-06 19:48:15 +0300

    Initial commit

---
+++ src/main/java/org/viktsh/MainClass.java
@@ -0,0 +1,13 @@
+package org.viktsh;
+
+/**
+ * Hello world!
+ *
+ */
+public class MainClass
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
